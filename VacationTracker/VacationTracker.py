ba10bcc91a1f7d473e11c45e9d085929258748a6
NikhilRaikar17 <nikhilraikar88@gmail.com>
2022-07-02 11:34:53 +0200

    adding new_customer and new_invoice items

--- VacationTracker/VacationTracker.py
+++ VacationTracker/VacationTracker.py
@@ -49,6 +49,47 @@ class Vacation(db.Model):
     StartDate = db.Column(db.Date)
     EndDate = db.Column(db.Date)
 
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):   
+        return True           
+
+    def is_anonymous(self):
+        return False          
+
+    def get_id(self):         
+        return str(self.id)
+
+class Customers(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(1000), nullable=False) 
+    email = db.Column(db.String(100), unique=True)
+    address = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(100), nullable=False)
+
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(100), unique=True)
+    price = db.Column(db.Float(100), nullable=False)
+class Invoice(db.Model):
+    __tablename__ = 'invoices'
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(100))
+    customer_id = db.Column(db.Integer,db.ForeignKey("customers.id"), nullable=False)
+    product_id = db.Column(db.Integer,db.ForeignKey("products.id"), nullable=False)
+
+    
 ##
 ## LOGIN SETUP
 ##
@@ -67,36 +108,13 @@ current_users = {}
 admin_users = {'danilo', 'holger', 'steffi', 'pvtest'}
 
 # Declare a flask_login compatible user data holder.
-class User(UserMixin):
-    def __init__(self, dn, username, data):
-        self.dn = dn
-        self.username = username
-        self.data = data
 
-    def __repr__(self):
-        return self.dn
-    
-    def get_id(self):
-        return self.dn
-    
-    def get_mail(self):
-        #return self.data['mail'][0] if 'mail' in self.data else None
-        return "pvtest@gmail.com"
-    
-    def get_name(self):
-        #return self.data['cn'][0] if 'cn' in self.data else self.username
-        return "pvtest"
-
-    def is_admin(self):
-        return "pvtest"
 
 # Declare the user loader callback for flask_login. Returns the user data for 
 # the given user_id. Returns None if no user with this user_id is logged in.
 @login_manager.user_loader
 def load_user(user_id):
-    if user_id in current_users:
-        return current_users[user_id]
-    return None
+    return Users.query.get(int(user_id))
 
 # Declare the user saver callback for flask_ldap3_login. This method is called 
 # whenever a LDAPLoginForm successfully validates. Saves the user, and returns 
@@ -125,106 +143,85 @@ def favicon():
 # login the user at this application.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LDAPLoginForm()
+    email = request.form.get('username')
+    password = request.form.get('password')
+    user = Users.query.filter_by(email=email).first()
     if request.method == 'POST':
-        if form.validate_on_submit():
-            login_user(form.user)
+        if user:
+            login_user(user)
             return redirect(url_for('index'))
         else:
             flash('Invalid username or password.')
-    return render_template('login.html', form = form)
+    return render_template('login.html')
 
 # Logout the current user.
 @app.route('/logout')
-
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
 # Show the vacation dates of the current user.
-@app.route('/vacation/<scope>')
+@app.route('/vacation')
+@login_required
+def vacation():
+    vacations = ''
+    return render_template("vacation.html", vacations = vacations)
 
-def vacation(scope):
-    if scope == 'all':
-        vacations = Vacation.query;
-    else:
-        vacations = Vacation.query.filter_by(Mail = "pvtest@gmail.com")
-    vacations = vacations.order_by(desc('StartDate'))
-    return render_template("vacation.html", vacations = vacations, scope = scope)
-
-
-# Add a new vacation date to the vacation database. The vacation start and
-# end dates are given.
-@app.route('/vacation_add/<scope>', methods = ['POST'])
-
-def vacation_add(scope):
-    startdate = datetime.strptime(request.form['add-start-date'], "%Y-%m-%d").date()
-    enddate = datetime.strptime(request.form['add-end-date'], "%Y-%m-%d").date()
-    if scope == 'all' and current_user.is_admin():
-        email = request.form['add-email']
-        if not ldap_manager.get_object(ldap_manager.full_user_search_dn, '(mail=' + email + ')', None):
-            flash('Unknown email address.')
-            # print('Unknown email address')
-            return redirect(url_for('vacation', scope = scope))
-    else:
-        email = current_user.get_mail()
-    vacation = Vacation(Mail = email, StartDate = startdate, EndDate = enddate)
-    db.session.add(vacation)
-    db.session.commit()
-    return redirect(url_for('vacation', scope = scope))
-
-# Update a vacation date. The vacation database entry id and the new vacation
-# start and end dates are given. 
-@app.route('/vacation_update/<scope>', methods = ['POST'])
-
-def vacation_update(scope):
-    if scope == 'all' and current_user.is_admin():
-        email = request.form['edit-email']
-        vacation = Vacation.query.filter_by(id = request.form['edit-vacation-id']).first()
-    else:
-        email = current_user.get_mail()
-        vacation = Vacation.query.filter_by(id = request.form['edit-vacation-id'], Mail = email).first()
-    if vacation != None:
-        vacation.Mail = email
-        vacation.StartDate = datetime.strptime(request.form['edit-start-date'], "%Y-%m-%d").date()
-        vacation.EndDate = datetime.strptime(request.form['edit-end-date'], "%Y-%m-%d").date()
-        db.session.commit()
-    return redirect(url_for('vacation', scope = scope))
-
-# Delete a vacation date. The vacation database entry id is given. 
-@app.route('/vacation_delete/<scope>', methods = ['POST'])
-
-def vacation_delete(scope):
-    if scope == 'all' and current_user.is_admin():
-        vacation = Vacation.query.filter_by(id = request.form['delete-vacation-id']).first()
-    else:
-        email = current_user.get_mail()
-        vacation = Vacation.query.filter_by(id = request.form['delete-vacation-id'], Mail = email).first()
-    if vacation != None:
-        db.session.delete(vacation)
-        db.session.commit()
-    return redirect(url_for('vacation', scope = scope))
-
-# Delete all vacation dates checked in the vacation dates table. The vacation
-# database entry ids are given.
-@app.route('/vacations_delete/<scope>', methods = ['POST'])
-
-def vacations_delete(scope):
-    for id in request.form['delete-vacation-ids'].split(','):
-        if scope == 'all' and current_user.is_admin():
-            vacation = Vacation.query.filter_by(id = id).first()
-        else:
-            vacation = Vacation.query.filter_by(id = id, Mail = current_user.get_mail()).first()
-        if vacation != None:
-            db.session.delete(vacation)
-    db.session.commit()
-    return redirect(url_for('vacation', scope = scope))
 
 @app.route('/generate_invoice')
-
+@login_required
 def generate_invoice():
-    vacations = Vacation.query.all()
+    vacations = ''
     return render_template("e-invoicegenerator.html", vacations = vacations, scope = "my")
+
+@app.route('/manage_customers', methods=['GET'])
+@login_required
+def manage_customers():
+    customers = Customers.query.all()
+    return render_template("customers.html", customers = customers)
+
+@app.route('/add_customer', methods=['POST'])
+@login_required
+def add_customer():
+    name = request.form.get('customer-name')
+    address = request.form.get('customer-address')
+    email = request.form.get('customer-email')
+    phone_number = request.form.get('customer-phone')
+    new_customer = Customers(name=name, address=address, email=email, phone_number=phone_number)
+    try:
+        db.session.add(new_customer)
+        db.session.commit()
+        flash("New customer successfully added", 'success')
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        db.session.flush()
+        flash("Some customer details entered are not proper", 'danger')
+    return redirect(url_for('manage_customers'))
+
+@app.route('/manage_invoice_items', methods=['GET'])
+@login_required
+def manage_invoice_items():
+    products = Products.query.all()
+    return render_template("invoice_items.html", products = products)
+
+@app.route('/add_product', methods=['POST'])
+@login_required
+def add_product():
+    name = request.form.get('item-name')
+    price = request.form.get('item-price')
+    new_product = Products(name=name, price=price)
+    try:
+        db.session.add(new_product)
+        db.session.commit()
+        flash("New item successfully added", 'success')
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+        db.session.flush()
+        flash("Item could not be added", 'danger')
+    return redirect(url_for('manage_invoice_items'))
 
 ##
 ## MAIN
